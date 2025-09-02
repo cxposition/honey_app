a6e37fda3d9d7e72ac62909ab3fe3aaca731a929
cxposition <2213087097@qq.com>
2025-09-02 18:25:59 +0800

    feat:封装公共分页查询参数部分

--- apps/honey_server/models/enter.go
+++ apps/honey_server/models/enter.go
@@ -11,3 +11,9 @@ type Model struct {
 	UpdatedAt time.Time      `json:"updatedAt"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt"`
 }
+
+type PageInfo struct {
+	Page  int    `form:"page"`
+	Limit int    `form:"limit"`
+	Key   string `form:"key"`
+}
