53f4c7dbd1d617fe5f134d8b738ec61f078ff9fd
cxposition <2213087097@qq.com>
2025-09-02 23:16:58 +0800

    feat:封装删除接口的请求公共函数

--- apps/honey_server/api/user_api/remove.go
+++ apps/honey_server/api/user_api/remove.go
@@ -3,9 +3,9 @@ package user_api
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"honey_app/apps/honey_server/global"
 	"honey_app/apps/honey_server/middleware"
 	"honey_app/apps/honey_server/models"
+	"honey_app/apps/honey_server/service/common_service"
 	"honey_app/apps/honey_server/utils/res"
 )
 
@@ -16,24 +16,36 @@ type UserRemoveRequest struct {
 func (UserApi) UserRemoveView(c *gin.Context) {
 	cr := middleware.GetBind[UserRemoveRequest](c)
 	log := middleware.GetLog(c)
-	var list []models.UserModel
-	global.DB.Find(&list, "id in ?", cr.IDList)
-	var successCount = int64(0)
-	if len(list) > 0 {
-		db := global.DB.Delete(&list)
-		err := db.Error
-		if err != nil {
-			msg := fmt.Sprintf("删除用户失败: %s", db.Error)
-			log.Errorf(msg)
-			log.Errorf("删除失败的入参 %v", cr.IDList)
-			res.FailWithMsg(msg, c)
-			return
-		}
-		successCount = db.RowsAffected
+	//var list []models.UserModel
+	//global.DB.Find(&list, "id in ?", cr.IDList)
+	//var successCount = int64(0)
+	//if len(list) > 0 {
+	//	db := global.DB.Delete(&list)
+	//	err := db.Error
+	//	if err != nil {
+	//		msg := fmt.Sprintf("删除用户失败: %s", db.Error)
+	//		log.Errorf(msg)
+	//		log.Errorf("删除失败的入参 %v", cr.IDList)
+	//		res.FailWithMsg(msg, c)
+	//		return
+	//	}
+	//	successCount = db.RowsAffected
+	//}
+	//
+	//msg := fmt.Sprintf("删除用户成功共%d个，成功%d个", successCount, len(list))
+	//log.Infof(msg)
+	//log.Infof("删除的用户信息%v", list)
+	successCount, err := common_service.Remove(models.UserModel{}, common_service.RemoveRequest{
+		Debug:  true,
+		IDList: cr.IDList,
+		Log:    log,
+		Msg:    "用户",
+	})
+	if err != nil {
+		msg := fmt.Sprintf("删除用户失败: %s", err)
+		res.FailWithMsg(msg, c)
+		return
 	}
-
-	msg := fmt.Sprintf("删除用户成功共%d个，成功%d个", successCount, len(list))
-	log.Infof(msg)
-	log.Infof("删除的用户信息%v", list)
+	msg := fmt.Sprintf("删除成功, 共%d个, 成功 %d 个", len(cr.IDList), successCount)
 	res.OkWithMsg(msg, c)
 }
---
+++ apps/honey_server/service/common_service/remove.go
@@ -0,0 +1,47 @@
+package common_service
+
+import (
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+	"honey_app/apps/honey_server/global"
+)
+
+type RemoveRequest struct {
+	Debug  bool
+	Where  *gorm.DB
+	IDList []uint
+	Log    *logrus.Entry
+	Msg    string
+}
+
+func Remove[T any](model T, req RemoveRequest) (successCount int64, err error) {
+	db := global.DB
+	if req.Debug {
+		db = db.Debug()
+	}
+	if req.Where != nil {
+		db = db.Where(req.Where)
+	}
+
+	db = db.Where(model)
+	if len(req.IDList) > 0 {
+		req.Log.Infof("删除 %s idList %v", req.Msg, req.IDList)
+		db = db.Where("id in ?", req.IDList)
+	}
+
+	var list []T
+	db.Find(&list)
+	if len(list) <= 0 {
+		req.Log.Infof("没查到")
+		return
+	}
+
+	result := global.DB.Delete(&list)
+	if result.Error != nil {
+		req.Log.Infof("删除失败 %s", result.Error)
+		return
+	}
+	successCount = result.RowsAffected
+	req.Log.Infof("删除 %s 成功 , 成功%v个", req.Msg, successCount)
+	return
+}
