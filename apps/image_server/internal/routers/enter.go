e0e9c40ca9d14df37e267c9c59a1dd336d6b0836
cxposition <2213087097@qq.com>
2025-09-05 05:58:27 +0800

    fix:修改import中的middleware1为middleware

--- apps/image_server/internal/routers/enter.go
+++ apps/image_server/internal/routers/enter.go
@@ -3,14 +3,14 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"image_server/internal/global"
-	middleware2 "image_server/internal/middleware"
+	"image_server/internal/middleware"
 )
 
 func Run() {
 	r := gin.Default()
 	r.Static("uploads", "uploads")
 	g := r.Group("honey_server")
-	g.Use(middleware2.LogMiddleware, middleware2.AuthMiddleware)
+	g.Use(middleware.LogMiddleware, middleware.AuthMiddleware)
 	webAddr := global.Config.System.WebAddr
 	r.Run(webAddr)
 }
