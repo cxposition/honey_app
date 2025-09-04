dde8092ea8b2e760b6e1c88c3f56f2078c8f9a1c
cxposition <2213087097@qq.com>
2025-09-04 08:19:35 +0800

    feat:添加操作日志

--- apps/honey_server/api/user_api/login.go
+++ apps/honey_server/api/user_api/login.go
@@ -6,8 +6,10 @@ import (
 	"honey_app/apps/honey_server/global"
 	"honey_app/apps/honey_server/middleware"
 	"honey_app/apps/honey_server/models"
+	"honey_app/apps/honey_server/service/log_service"
 	"honey_app/apps/honey_server/utils/captcha"
 	"honey_app/apps/honey_server/utils/jwts"
+	"honey_app/apps/honey_server/utils/pwd"
 	"honey_app/apps/honey_server/utils/res"
 )
 
@@ -20,17 +22,27 @@ type LoginRequest struct {
 
 func (UserApi) LoginView(c *gin.Context) {
 	cr := middleware.GetBind[LoginRequest](c)
+	LoginLog := log_service.NewSuccessLog(c)
 	if cr.CaptchaID == "" || cr.CaptchaCode == "" {
+		LoginLog.FailLog(cr.Username, "", "未输入图片验证码")
 		res.FailWithMsg("请输入图片验证码", c)
 		return
 	}
 	if !captcha.CaptchaStore.Verify(cr.CaptchaID, cr.CaptchaCode, true) {
+		LoginLog.FailLog(cr.Username, "", "图片验证码验证失败")
 		res.FailWithMsg("图片验证码验证失败", c)
 		return
 	}
 	var user models.UserModel
 	err := global.DB.Take(&user, "username = ?", cr.Username).Error
 	if err != nil {
+		LoginLog.FailLog(cr.Username, cr.Password, "用户名不存在")
+		res.FailWithMsg("用户名或密码错误", c)
+		return
+	}
+
+	if !pwd.CompareHashAndPassword(user.Password, cr.Password) {
+		LoginLog.FailLog(cr.Username, cr.Password, "密码错误")
 		res.FailWithMsg("用户名或密码错误", c)
 		return
 	}
@@ -45,5 +57,6 @@ func (UserApi) LoginView(c *gin.Context) {
 		return
 	}
 
+	LoginLog.SuccessLog(user.ID, user.Username)
 	res.OkWithData(token, c)
 }
