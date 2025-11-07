c34fdaa95833ada68e8ce456bd64e7f636baf04d
cxposition <2213087097@qq.com>
2025-11-07 11:09:59 +0800

    feat:完成诱捕ip删除接口（部分实现）

--- apps/honey_server/internal/api/honey_ip_api/remove.go
+++ apps/honey_server/internal/api/honey_ip_api/remove.go
@@ -1,1 +1,40 @@
 package honey_ip_api
+
+import (
+	"github.com/gin-gonic/gin"
+	"honey_server/internal/global"
+	"honey_server/internal/middleware"
+	"honey_server/internal/models"
+	"honey_server/internal/service/grpc_service"
+	"honey_server/internal/utils/res"
+)
+
+func (HoneyIPApi) RemoveView(c *gin.Context) {
+	cr := middleware.GetBind[models.IDRequestList](c)
+	var honeyIPList []models.HoneyIpModel
+	global.DB.Preload("NodeModel").Find(&honeyIPList, "id in ?", cr.IdList)
+	if len(honeyIPList) == 0 {
+		res.FailWithMsg("未找到诱捕ip", c)
+		return
+	}
+
+	nodeModel := honeyIPList[0].NodeModel
+	// 判断节点是否在线
+	if nodeModel.Status != 1 {
+		res.FailWithMsg("节点未运行", c)
+		return
+	}
+
+	// 使用封装的获取节点函数
+	_, ok := grpc_service.GetNodeCommand(nodeModel.Uid)
+	if !ok {
+		res.FailWithMsg("节点离线中", c)
+		return
+	}
+
+	// 下发批量删除的任务到消息队列
+
+	// 改状态
+	global.DB.Model(&honeyIPList).Update("status", 4)
+	res.OkWithMsg("批量删除中", c)
+}
--- apps/honey_server/internal/routers/honey_ip_routers.go
+++ apps/honey_server/internal/routers/honey_ip_routers.go
@@ -5,10 +5,12 @@ import (
 	"honey_server/internal/api"
 	"honey_server/internal/api/honey_ip_api"
 	"honey_server/internal/middleware"
+	"honey_server/internal/models"
 )
 
 func HoneyIPRouters(r *gin.RouterGroup) {
 	app := api.App.HoneyIPApi
 	r.POST("honey_ip", middleware.BindJsonMiddleware[honey_ip_api.CreateRequest], app.CreateView)
 	r.GET("honey_ip", middleware.BindQueryMiddleware[honey_ip_api.ListRequest], app.ListView)
+	r.DELETE("honey_ip", middleware.BindJsonMiddleware[models.IDRequestList], app.RemoveView)
 }
